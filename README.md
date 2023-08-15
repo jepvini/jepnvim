070f07d9f7718e6d6e56b9a7d509ecb7620fecf9
leo359531 <leos1359531@gmail.com>
2023-08-15 14:56:57 +0200

    ()

---
+++ README.md
@@ -0,0 +1,225 @@
+# NVIM config in lua
+
+*by jep*
+
+### Dependencies:
+
+nix -> for **lazy-lsp**
+
+it can be installed running\
+`$ curl -L https://nixos.org/nix/install | sh`
+
+Dictionary can also be added for autocompletion, just place them in the `dicts` folder and add them with the `nvim-cmp` plugin
+
+### Usage:
+
+The leader key is `space`
+
+In order to navigate pls don't be a n**b and use `hjkl`, this is not VScode
+
+`Up` and `Down` keys have been disabled since they are sent by the terminal when the touchpad is accidentally touched while typing -- really annoying
+
+### Keybindings
+
+Disclaimer: The key notation is the same used by vim
+- `a` -> press a
+- `<leader>` -> press the leader key
+- `<leader>a` -> press the leader key then a 
+- `<C-a>` -> ctrl-a, at the same time
+
+all the Keybindings are in normal mode, unless specified
+
+### Base
+
+- save file: `<leader>w`
+- exit file without closing the window: `<leader>q`
+- esc: `jk` -> insert mode
+
++ auto-indent all file: `<leader>g` 
++ next tab: `<leader>tl`
++ prev tab: `<leader>th`
+
+- comment current line: `<leader>cc`
+- add comment at the end of the line: `<leader>cA`
+- commedt visual selected block: `<leader>b`
+
++ sort alphanumerically visual selected code: `<leader>s`
+
+- yank to system keyboard: `cs`
+- paste from system keyboard: `cv`
+
+### Plugins
+
+**nvim-tree** -> file manager
+- toggle open-close: `<leader>e`
+- expand folder or go to file: `L` *the file is open in a new tab*
+- back: `H` 
+- toggle hidden files: `gh`
+
+**telescope** -> fuzzy searcher
+- search file in current buffer: `<leader><space>` *double tap space*
+- search in reccent files: `<leader>r`
+- search files in current dir: `<leader>ff`
+- grep current dir: `<leader>fg` *uses ripgrep, written in rust:)*
+- search in current file: `<leader>fs` *use this instead as classic vim `/`
+
++ open file in a new tab: `<C-t>`
++ open file in split: `<C-x>`
++ open file in vsplit: `<C-v>`
+
+**toggle terminal** -> terminal (really???)
+- toggle open-close: `<C-t>`
+
+**nvim-cmp** -> autocomplete
+- select highlighted: `<CR>` -> enter key
+- next: `<Tab>`
+- prev: `<S-Tab>`
+- close: `<C-e>`
+- show all words `<C-Space>` -> eng words appear only with more than 6 chars normally
+
+**vim-surround** -> manages quotes, brackets etc
+- add surround: `ys`
+- rm surround: `rs`
+- replace surround: `cs`
+
+of what:
++ inner word (bounded by non letters): `iw`
++ inner Word (bounded by spaces): `iW`
++ etc. -> more in vim documentation 
+
+what:
+- quotes: `'`
+- for replace: `'"` -> single with double
+
+to replace single quotes with double of a Word is: `csiW'"`
+
+### Useful vim std Keybindings
+
+**Nice to know**
+- repeat last action: `.`
+- do action *n* times: `*n*<action>`
+- undo: `u`
+- redo: `<C-r>`
+- merge two lines: `<S-j>`
+
+delete 3 lines: `3dd` -> for more info about dd keep reading
+
+**Commands** -> what you want to do
++ yank: `y`
++ paste: `p`
++ change: `c` -> delete and go in insert mode
+
+- delete char under the cursor: `x` -> no select needed
+- delete: `d`
+
++ capitalize: `gU`
++ make lowercase: `gu`
+
+- find char next: `f<char>` -> no select needed
+- find char prev: `F<char>` -> no select needed
+
+**Select** -> on what
+char
+- under the cursor: `l`
+- prev: `h`
+
+word -> delimited by spaces or punctuation
+- word from cursor to punctuation or space (included): `w`
+- word from cursor to last char: `e` -> end
+- word from cursor to first char: `b`
+
+bigword -> delimited by spaces
+*same as word but capitalized*
+
+line
+- from cursor to end: `$`
+- from cursor to beginning: `0`
+- from cursor to first non blank of the line: `^`
+
+for current line just repeat the last letter:
+- yank line: `yy`
+- delete line: `dd`
+- change line: `cc`
+- capitalize line: `gUU` 
+
+others lines:
+- current line and previous: `k`
+- current line and next: `j`
+
+in and out:
+- select whole word under the cursor: `iw`
+- select whole word with surrounding spaces under the cursor: `aw`
+- select text between quotes (without the quotes): `i'`
+- select text between quotes (with the quotes): `a'`
+
+whole file:
+- top: `gg`
+- bottom: `G`
+ 
+**Move the cursor**
+- page Up: `<C-f>` -> forward
+- page Down: `<C-b>` -> back
+
+- goto line: `3G` -> goto line 3
+- goto top: `gg`
+- goto EOF: `G`
+
+all the inline movement are the same used for the **select section**
+- goto next word: `w`
+- goto end of line `$`
+etc.
+
+insert
++ before cursor: `i`
++ after cursor: `a`
++ to end of line: `A`
++ to beginning of line: `I`
++ black line below: `o`
++ blank line above: `O`
+
+**visual mode** -> the most precise way of selecting things
+- visual select char: `v`
+- visual select whole line: `V`
+- visual select block: `<C-v>`
+
++ indent selected: `>`
++ remove indentation selected: `<`
+
+- insert in multiple lines: `<C-v><select lines><S-i><esc>`
+
+**Panes**
+- select pane mode: `<C-w>` -> then choose a direction using `hjkl`
+
+**Macro** -> run the same list of actions multiple times
+- record: `q`
+- select buffer: `a` *for example*
+- run commands
+- stop recording: `q`
+- run macro `@a` -> run macro in buffer a
+
+**Search and replace**
++ in line first occurrence: `:s/foo/new_foo/`
++ in line all occurrence: `:s/foo/new_foo/g`
++ all file all occurrence: `:%s/foo/new_foo/`
+
+**Change case**
+- low to up: `gU`
+- up to low: `gu`
+- then select what you want to change
+- selected char: `gUl`
+- whole word: `gUiw`
+
+**Auto indent**
++ indent:`=`
++ what:`G` -> till EOF
+so to indent whole file and don't lose the cursor position: `gg=G''`
+
+**Registers** -> vim has multiple keyboard registers
+- select register key: `"`
+
+numerical registers are used for yank and delete
+`0` is the last register, then `1` and so on
+
+`+` is the system buffer
+yank line to system buffer: `"+yy`
+paste from system buffer: `"+p` -> although this works `Control-Shift-v` is more convenient 
---
+++ init.lua
@@ -0,0 +1,490 @@
+-- My nvim config in lua 
+-- By  Jep
+--
+-- stil wip as always 
+--
+-- Dependencies:
+--
+-- nix -> for lazy-lsp
+-- $ curl -L https://nixos.org/nix/install | sh
+--
+-- english dictionary 
+-- install aspell
+-- # apt install aspell aspell-en
+--
+-- pipe it to a location
+-- $ aspell -d en dump master | aspell -l en expand > my.dict
+
+
+--------------------------------------------------
+
+-- vim.opt settings
+
+--------------------------------------------------
+
+-- Line numbers
+vim.opt.number = true
+
+-- Relative numbers
+vim.wo.relativenumber = true
+
+-- Ignore case in search
+vim.opt.ignorecase = true
+
+-- Search upper case
+vim.opt.smartcase = true
+
+-- Remove previus search highlihts
+vim.opt.hlsearch = false
+
+-- Long lines wrapped
+vim.opt.wrap = true
+
+-- Preserve indentation with wrapped lines
+vim.opt.breakindent = true
+
+-- Tab size
+vim.opt.tabstop = 2
+
+-- Indent size
+vim.opt.shiftwidth = 2
+
+-- Tab are spaces
+vim.opt.expandtab = true
+
+-- No mouse in insert mode
+vim.opt.mouse = 'n'
+
+----------------------------------------------------
+
+-- Keybindings
+
+----------------------------------------------------
+
+-- Leader
+vim.g.mapleader = ' '
+
+-- jk as esc
+vim.keymap.set({'i', 'v'}, 'jk', '<esc>')
+
+-- Save file
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
+
+-- Quit file
+vim.keymap.set('n', '<leader>q','<cmd>exit<cr>')
+
+-- Copy and paste to/from keyboard
+vim.keymap.set({'n', 'x', 'v'}, 'cp', '"+y')
+vim.keymap.set({'n', 'x', 'v'}, 'cv', '"+p')
+
+--- Delete does not change internal register
+vim.keymap.set({'n', 'x', 'v'}, 'x', '"_x')
+
+-- Sort selected text
+vim.keymap.set('v', '<leader>S', ':sort<cr>')
+
+-- Select whole buffer
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
+
+-- Indent whole file
+vim.keymap.set('n', '<leader>g', "gg=G''")
+
+-- Tabs
+vim.keymap.set('n', '<leader>tl', '<cmd>+tabnext<cr>') -- next
+vim.keymap.set('n', '<leader>th', '<cmd>-tabnext<cr>') -- prev
+
+-- Disable scroll in insert mode
+vim.keymap.set('i', '<Up>', '<nop>')
+vim.keymap.set('i', '<Down>', '<nop>')
+vim.keymap.set('n', '<Up>', '<nop>')
+vim.keymap.set('n', '<Down>', '<nop>')
+vim.keymap.set('v', '<Up>', '<nop>')
+vim.keymap.set('v', '<Down>', '<nop>')
+
+-- Toggle first letter of word upper case or lower case
+vim.keymap.set('n', '<leader>U', 'viWo<esc>gUl')
+vim.keymap.set('n', '<leader>u', 'viWo<esc>gul')
+
+
+
+----------------------------------------------------
+
+-- Plugins
+
+----------------------------------------------------
+local lazy = {}
+
+function lazy.install(path)
+  if not vim.loop.fs_stat(path) then
+    print('Installing lazy.nvim....')
+    vim.fn.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      path,
+    })
+  end
+end
+
+function lazy.setup(plugins)
+  -- You can "comment out" the line below after lazy.nvim is installed
+  lazy.install(lazy.path)
+
+  vim.opt.rtp:prepend(lazy.path)
+  require('lazy').setup(plugins, lazy.opts)
+end
+
+lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+lazy.opts = {}
+
+-- Plugin space
+
+lazy.setup({
+  -- Themes
+  { 'folke/tokyonight.nvim', priority = 10000 },
+  { 'ellisonleao/gruvbox.nvim', priority = 1000 },
+  { 'rebelot/kanagawa.nvim', priority = 1000 },
+  -- Various
+  { 'LnL7/vim-nix' },
+  { 'akinsho/toggleterm.nvim' },
+  { 'editorconfig/editorconfig-vim' },
+  { 'kyazdani42/nvim-tree.lua' },
+  { 'lewis6991/gitsigns.nvim' },
+  { 'lukas-reineke/indent-blankline.nvim' },
+  { 'norcalli/nvim-colorizer.lua' },
+  { 'numToStr/Comment.nvim' },
+  { 'nvim-lualine/lualine.nvim' },
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.2', dependencies = { 'nvim-lua/plenary.nvim' }},
+  { 'nvim-tree/nvim-web-devicons' },
+  { 'nvim-treesitter/nvim-treesitter' },
+  { 'pocco81/auto-save.nvim' },
+  { 'tpope/vim-repeat' },
+  { 'tpope/vim-surround' },
+  { 'wellle/targets.vim' },
+  { 'zhimsel/vim-stay' },
+  {"ellisonleao/glow.nvim", config = true, cmd = "Glow"},
+
+  -- StartUp
+  { "startup-nvim/startup.nvim" },
+
+  -- Lsp
+  { 'dundalek/lazy-lsp.nvim' },
+  { 'neovim/nvim-lspconfig' },
+
+  -- Autocomplete
+  { 'hrsh7th/cmp-buffer' }, -- buffer
+  { 'hrsh7th/cmp-nvim-lsp' }, -- Lsp
+  { 'hrsh7th/cmp-path' }, -- path auto
+  { 'hrsh7th/cmp-vsnip' }, --snips
+  { 'hrsh7th/nvim-cmp' }, -- main
+  { 'hrsh7th/vim-vsnip' }, --snip source
+  { 'onsails/lspkind.nvim' }, -- vscode like pictogram
+  { 'uga-rosa/cmp-dictionary' }, -- dictionary auto
+
+})
+
+-- Plugin config
+
+-- Tokyonight
+-- vim.opt.termguicolors = true
+-- vim.cmd.colorscheme('tokyonight')
+
+-- Gruvbox
+-- vim.o.background = "dark" -- or "light" for light mode
+-- vim.cmd([[colorscheme gruvbox]])
+
+-- Kanagawa
+require('kanagawa').setup({
+  background = {               -- map the value of 'background' option to a theme
+    dark = "wave",           -- try "dragon" !
+    light = "lotus"
+  },
+})
+
+vim.cmd.colorscheme('kanagawa')
+
+-- Lualine
+vim.opt.showmode = false
+
+require('lualine').setup({
+  options = {
+    theme = 'auto',
+    icons_enabled = true,
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {'NvimTree'}
+    }
+  },
+})
+
+-- Nvim web devicons
+require'nvim-web-devicons'.setup {}
+
+-- Indent-Blankline
+vim.opt.list = true
+vim.opt.listchars:append "eol:↴"
+
+require("indent_blankline").setup {
+  space_char_blankline = " ",
+  show_current_context = true,
+  show_current_context_start = false,
+}
+
+-- Treesitter
+require('nvim-treesitter.configs').setup({
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true
+  },
+  ensure_installed = {
+    'bash',
+    'c',
+    'c_sharp',
+    'comment',
+    'cpp',
+    'css',
+    'dockerfile',
+    'fish',
+    'git_config',
+    'git_rebase',
+    'gitattributes',
+    'gitcommit',
+    'gitignore',
+    'html',
+    'ini',
+    'javascript',
+    'json',
+    'latex',
+    'lua',
+    'markdown',
+    'markdown_inline',
+    'matlab',
+    'nix',
+    'python',
+    'rust',
+    'yaml',
+  },
+})
+
+-- Targets
+vim.g.targets_aiAI = 'aiAI'
+
+-- Comment
+require('Comment').setup({
+  toggler = {
+    ---Line-comment toggle keymap
+    line = '<leader>cc',
+    ---Block-comment toggle keymap
+    block = '<leader>bc',
+  },
+  opleader = {
+    ---Line-comment keymap
+    line = '<leader>c',
+    ---Block-comment keymap
+    block = '<leader>b',
+  },
+  extra = {
+    ---Add comment on the line above
+    above = '<leader>cO',
+    ---Add comment on the line below
+    below = '<leader>co',
+    ---Add comment at the end of line
+    eol = '<leader>cA',
+  },
+})
+
+-- Nvim tree
+require('nvim-tree').setup({
+  hijack_cursor = false,
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
+  },
+  on_attach = function(bufnr)
+    local bufmap = function(lhs, rhs, desc)
+      vim.keymap.set('n', lhs, rhs, {buffer = bufnr, desc = desc})
+    end
+
+    -- See :help nvim-tree.api
+    local api = require('nvim-tree.api')
+
+    bufmap('L', api.node.open.tab, 'Expand folder or go to file')
+    bufmap('H', api.node.navigate.parent_close, 'Close parent folder')
+    bufmap('gh', api.tree.toggle_hidden_filter, 'Toggle hidden files')
+  end
+})
+
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>')
+
+-- Telescope
+vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', '<leader>r', '<cmd>Telescope oldfiles<cr>')
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
+
+-- Toggleterminal
+require('toggleterm').setup({
+  open_mapping = '<C-t>',
+  direction = 'horizontal',
+  shade_terminals = true
+})
+
+-- Gitsigns
+require('gitsigns').setup({
+  signs = {
+    add = {text = '▎'},
+    change = {text = '▎'},
+    delete = {text = '➤'},
+    topdelete = {text = '➤'},
+    changedelete = {text = '▎'},
+  }
+})
+
+-- Colorizer
+require'colorizer'.setup()
+
+-- Glow
+require('glow').setup({
+  style = "dark",
+  width = 120,
+})
+
+-- Plugins end
+
+
+
+----------------------------------------------------
+
+-- StartUp
+
+----------------------------------------------------
+
+require("startup").setup({theme = "startify" })
+
+
+
+----------------------------------------------------
+
+-- LSP servers with lazy-lsp
+
+----------------------------------------------------
+
+require("lazy-lsp").setup {
+  excluded_servers = {
+    "sqls"
+  },
+  preferred_servers = {
+    haskell = { "hls" },
+    rust = { "rust_analyzer" },
+  },
+  configs = {
+    lua_ls = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = { "vim" },
+          },
+        },
+      },
+    },
+  },
+}
+
+
+
+----------------------------------------------------
+
+-- Autocompletion
+
+----------------------------------------------------
+
+-- Set up nvim-cmp.
+
+local kind_icons = {
+  Text = "",
+  Method = "",
+  Constructor = "",
+}
+
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+    ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+  },
+  formatting = {
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      -- Source
+      vim_item.menu = ({
+        buffer = "[Buffer]",
+        vsnip = "[Vsnip]",
+        nvim_lsp = "[LSP]",
+        path = "[Path]",
+        dictionary = "[Dict]",
+      })[entry.source.name]
+      return vim_item
+    end
+  },
+  sources = {
+    { name = 'buffer' },
+    { name = "vsnip" },
+    { name = "nvim_lsp" },
+    { name = "path" },
+    {
+      name = "dictionary",
+      keyword_length = 5,
+    },
+  },
+})
+
+-- Dictionary setup
+--
+-- to change dict just run 
+-- :set spelllang=en
+local dict = require("cmp_dictionary")
+
+dict.setup({
+  -- The following are default values.
+  exact = 2,
+  first_case_insensitive = false,
+  document = false,
+  document_command = "wn %s -over",
+  async = false,
+  sqlite = false,
+  max_items = -1,
+  capacity = 5,
+  debug = false,
+})
+
+dict.switcher({
+  spelllang = {
+    en = "~/.config/nvim/dicts/en.dict",
+  },
+})
