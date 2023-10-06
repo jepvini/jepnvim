abcbc6ab28251fd1e9ec8141e93639033f1f0061
leo359531 <leos1359531@gmail.com>
2023-10-06 12:38:20 +0200

    ()

--- init.lua
+++ init.lua
@@ -330,6 +330,15 @@ require("nvim-treesitter.configs").setup({
 		"rust",
 		"yaml",
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<CR>",
+			scope_incremental = "<CR>",
+			node_incremental = "<TAB>",
+			node_decremental = "<S-TAB>",
+		},
+	},
 })
 
 -- Targets
